b5423cb0bb2975ce48cb9e41b36b66c7fd9681d3
Daniel Destouche <daniel.destouche@gmail.com>
2025-10-30 01:37:10 -0400

    feat: Package updates to prep for publish

--- src/ILineWriter.cs
+++ src/ILineWriter.cs
@@ -1,6 +1,6 @@
 ﻿namespace Toon
 {
-    public interface ILineWriter
+    internal interface ILineWriter
     {
         void Push(string content, int depth);
         void Push(char content, int depth);
